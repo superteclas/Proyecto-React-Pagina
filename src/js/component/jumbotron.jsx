d21e92fc4da46a0e85c12f6511d1344b72efa4ab
Alvaro Ponce <alvaroponce@iMac-de-Alvaro-2.local>
2024-02-29 18:16:06 +0100

    nav acabadoo

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -2,12 +2,11 @@ import React from "react";
 
 const Jumbotron = () => {
   return (
-    <div className="jumbotron custom-jumbotron">
-      <h1 className="display-4">A Warm Welcome</h1>
-      <p className="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero
-aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
+    <div className="jumbotron custom-jumbotron" style={{ marginLeft: '1em' }}>
+      <h1 className="display-4 custom-h1">A Warm Welcome!</h1>
+      <p className="lead custom-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
       
-      <a className="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
+      <a className="btn btn-primary btn-lg custom-button" href="#" role="button">Call to action!</a>
     </div>
   );
 };
