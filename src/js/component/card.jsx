a74374af4c5d6fddaf8fb531d9d8bed6d4094e81
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-03-01 18:57:22 +0100

    faltan arreglar las cards y el navvar posicion

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,11 +1,9 @@
-// card.js
 import React from 'react';
 
-
-const Card = ({ titulo,texto,boton }) => {
+const Card = ({ titulo, texto, boton }) => {
   return (
-    <div className="card custom-card" style={{ width: '12rem', margin: '0.1rem' }}>
-      <img src="https://plus.unsplash.com/premium_photo-1706142540630-51555d91b574?q=80&w=1925&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="..." />
+    <div className="card custom-card" style={{ width: '100%', margin: '0.5rem' }}>
+      <img src="https://via.placeholder.com/500" className="card-img-top" alt="..." />
       <div className="card-body d-flex flex-column justify-content-between text-center">
         <h5 className="card-title">{titulo}</h5>
         <p className="card-text">{texto}</p>
