e5850cc718fc1e3c80d31e079f5cbba3ebb1a45c
Alvaro Ponce <alvaroponce@iMac-de-Alvaro-2.local>
2024-03-01 08:16:19 +0100

    casi acabado

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = () => {
   return (
-    <div className="card custom-card" style={{ width: '12rem', margin: '0.5rem' }}>
+    <div className="card custom-card" style={{ width: '12rem' ,margin: '0.1rem' }}>
       <img src="https://plus.unsplash.com/premium_photo-1706142540630-51555d91b574?q=80&w=1925&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="..." />
       <div className="card-body d-flex flex-column justify-content-between text-center">
         <h5 className="card-title">Card title</h5>
