67de9384232a0ca86455b38161a4c360380cb9ee
Alvaro Ponce <alvaroponce@iMac-de-Alvaro-2.local>
2024-03-01 14:04:34 +0100

    funciona los props

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -5,12 +5,13 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-body-tertiary">
       <div className="container-fluid">
+       
         <a className="navbar-brand custom-text" href="#">Start Bootstrap</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link white-text" aria-current="page" href="#">Home</a>
             </li>
